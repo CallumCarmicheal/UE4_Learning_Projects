418290eea716b39806d618d396d102481e5d649b
Callum Carmicheal <callumcarmicheal@gmail.com>
2019-02-08 09:24:52 +0000

    Initial Commit of all assets

---
+++ Source/FPSGame/Private/FPSBlackhole.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSBlackhole.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SphereComponent.h"
+#include "Components/PrimitiveComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/World.h"
+#include "Engine/GameEngine.h"
+
+// Sets default values
+AFPSBlackhole::AFPSBlackhole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = MeshComp;
+
+	InnerSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("InnerSphereComp"));
+	InnerSphereComponent->SetSphereRadius(100);
+	InnerSphereComponent->SetupAttachment(MeshComp);
+	InnerSphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AFPSBlackhole::OverlapInnerSphere);
+	
+	OuterSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("OuterSphereComp"));
+	OuterSphereComponent->SetSphereRadius(3000);
+	OuterSphereComponent->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AFPSBlackhole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFPSBlackhole::OverlapInnerSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+		OtherActor->Destroy();
+}
+
+// Called every frame
+void AFPSBlackhole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Find all overlapping components that can collide and may be physically simulating
+	TArray<UPrimitiveComponent*> taOverlappingComponents;
+	OuterSphereComponent->GetOverlappingComponents(taOverlappingComponents);
+
+	for (int i = 0; i < taOverlappingComponents.Num(); i++) {
+		UPrimitiveComponent* actor = taOverlappingComponents[i];
+
+		if (actor && actor->IsSimulatingPhysics()) {
+			// The component we are looking for! It needs to be simulating in order to apply forces.
+
+			const float SphereRadius = OuterSphereComponent->GetScaledSphereRadius();
+			const float ForceStrength = -2000; // Negative value to make it pull towards the origin instead of pushing
+
+			actor->AddRadialForce(GetActorLocation(), SphereRadius, ForceStrength, ERadialImpulseFalloff::RIF_Constant, true);
+		}
+	}
+}
---
+++ Source/FPSGame/Public/FPSBlackhole.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSBlackhole.generated.h"
+
+class USphereComponent;
+class UPrimitiveComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class FPSGAME_API AFPSBlackhole : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFPSBlackhole();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* InnerSphereComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* OuterSphereComponent;
+
+	UFUNCTION()
+	void OverlapInnerSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditInstanceOnly, Category = "Blackhole")
+	bool IsEnabled;
+	
+};
