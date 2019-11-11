2e6d1b117f0d0e1d3ff0d970257d56f1f7e524e0
Callum Carmicheal <callumcarmicheal@gmail.com>
2019-11-11 12:24:04 +0000

    Create FPSPlayerController

--- Source/FPSGame/Public/GameVersion.generated.h
+++ Source/FPSGame/Public/GameVersion.generated.h
@@ -6,9 +6,9 @@
 #define BUILD_PATCH_VERSION 0
 
 // Build
-#define BUILD_BUILD_NUMBER 117
+#define BUILD_BUILD_NUMBER 123
 #define BUILD_BUILD_DATE "Monday, 11 November 2019"
-#define BUILD_BUILD_TIME "11:55:46"
+#define BUILD_BUILD_TIME "12:15:56"
 
 // Git
-#define BUILD_GIT_HASH "3ae3e12"
+#define BUILD_GIT_HASH "c60cb95"
